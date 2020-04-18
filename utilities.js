75101dd3fdbd27519f677ed2415b3e0a2e5ea733
TheBodger <41484656+TheBodger@users.noreply.github.com>
2020-04-18 18:44:47 +0100

    Create utilities.js

---
+++ utilities.js
@@ -0,0 +1,10 @@
+	showElapsed: function () {
+		endTime = new Date();
+		var timeDiff = endTime - startTime; //in ms
+		// strip the ms
+		timeDiff /= 1000;
+
+		// get seconds 
+		var seconds = Math.round(timeDiff);
+		return (" " + seconds + " seconds");
+	},
