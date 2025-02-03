ce7b68bae27b9b488940e64ca0e5c691add039b1
TheBodger <41484656+TheBodger@users.noreply.github.com>
2025-02-03 16:49:11 +0000

    Amended to support docker install

--- install.sh
+++ install.sh
@@ -3,29 +3,9 @@
 
 #dont forget the chmod +x run this in bash land
 
-#go back to modules
-cd ../
+#to support docker installs, the npm modules need to be installed from the feedutils module directory
 
-#
-#rm -r MMM-FeedDisplay
-git clone https://github.com/TheBodger/MMM-FeedDisplay
-
-
-
-#rm -r MMM-FeedProvider-RSS
-git clone https://github.com/TheBodger/MMM-FeedProvider-RSS
-#rm -r MMM-FeedProvider-Twitter
-#git clone https://github.com/TheBodger/MMM-FeedProvider-Twitter #no longer supported
-#rm -r MMM-FeedProvider-Instagram
-git clone https://github.com/TheBodger/MMM-FeedProvider-Instagram
-#rm -r MMM-FeedProvider-Reddit
-git clone https://github.com/TheBodger/MMM-FeedProvider-Reddit
-
-#go back to mm root
-
-cd ../
-
-#install all the dependencies here so we don't create humongous directories of copies of npm modules
+#install all the dependencies here 
 
 npm install axios
 npm install winston
@@ -36,3 +16,19 @@ npm install htmlparser2
 npm install isprofanity
 npm install xmlhttprequest
 npm install leo-profanity
+
+#go back to modules
+cd ../
+
+#
+#rm -r MMM-FeedDisplay
+git clone https://github.com/TheBodger/MMM-FeedDisplay
+#rm -r MMM-FeedProvider-RSS
+git clone https://github.com/TheBodger/MMM-FeedProvider-RSS
+#rm -r MMM-FeedProvider-Twitter
+#git clone https://github.com/TheBodger/MMM-FeedProvider-Twitter #no longer supported
+#rm -r MMM-FeedProvider-Instagram
+git clone https://github.com/TheBodger/MMM-FeedProvider-Instagram
+#rm -r MMM-FeedProvider-Reddit
+git clone https://github.com/TheBodger/MMM-FeedProvider-Reddit
+
